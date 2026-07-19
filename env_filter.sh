1214f5052d406a3d7a11c825d2d27e3432f87c64
PVM-05 <pvmthere@gmail.com>
2026-07-19 15:05:48 +0700

    chore(cleanup): remove redundant test and scratch files from repository

--- env_filter.sh
+++
@@ -1,9 +0,0 @@
-
-if [ "$GIT_COMMITTER_EMAIL" = "matt_parker@outlook.com" ] || [ "$GIT_COMMITTER_EMAIL" = "matt@mavenimaging.com" ]; then
-    export GIT_COMMITTER_NAME="PVM-05"
-    export GIT_COMMITTER_EMAIL="pvmthere@gmail.com"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "matt_parker@outlook.com" ] || [ "$GIT_AUTHOR_EMAIL" = "matt@mavenimaging.com" ]; then
-    export GIT_AUTHOR_NAME="PVM-05"
-    export GIT_AUTHOR_EMAIL="pvmthere@gmail.com"
-fi
